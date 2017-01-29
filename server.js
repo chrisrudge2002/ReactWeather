fa8a6b076eeef4d9c14c91813a64f486ab891860
Chris Rudge <chrisrudge2002@gmail.com>
2017-01-29 12:18:29 +0000

    Prep repo for heroku

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const PORT = process.env.PORT || 3000;
 // Create our app
 let app = express();
 
+// Re-direct all https traffic to use http
+app.use(function(req, res, next) {
+	if (req.headers['x-forwarded-proto'] === 'http') {
+		next();
+	} else {
+		res.redirect(`http://${req.hostname}${req.url}`);
+	}
+});
+
 app.use(express.static('public'));
 
 app.listen(PORT, function() {
