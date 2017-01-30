b4a3e7f176431fa2143be7feb8394fa20b3f3397
Chris Rudge <chrisrudge2002@gmail.com>
2017-01-30 18:10:21 +0000

    Add custom styles to h1 and search elements

--- app/app.jsx
+++ app/app.jsx
@@ -11,6 +11,9 @@ const Weather = require('Weather');
 require('style!css!foundation-sites/dist/css/foundation.min.css');
 $(document).foundation();
 
+// App css
+require('style!css!applicationStyles');
+
 ReactDOM.render(
 	<Router history={hashHistory}>
 		<Route path="/" component={Main}>
--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,7 @@ const {Link} = require('react-router');
 const About = (props) => {
 	return (
 		<div>
-			<h1 className="text-center">About</h1>
+			<h1 className="text-center page-title">About</h1>
 			<p>This is a weather application built on React. I have built this for The Complete React Web App Developer Course.</p>
 			<p>Here are some of the tools I used:</p>
 			<ul>
